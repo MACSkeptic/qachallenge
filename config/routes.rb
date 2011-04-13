74e9e8d4ed9db2cd371f32f0a89bde762dc073e6
macskeptic <macskeptic@gmail.com>
2011-04-13 00:36:30 -0300

    adding a homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 TwQaThingy::Application.routes.draw do
   resources :interests
-
   resources :users
+  root :to => "welcome#index"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
